cad450f247434d452a3610b92d5519105d44cae0
StrongholdOverlordsMod <strongholdoverlordsmod@gmail.com>
2021-08-07 15:43:59 +0200

    Added new class to store basic app config

---
+++ Gm1KonverterCrossPlatform/HelperClasses/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Gm1KonverterCrossPlatform.HelperClasses
+{
+	internal static class Config
+	{
+		internal const string AppName = "Gm1ConverterCrossPlatform";
+
+		internal static readonly string AppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName);
+		internal static readonly string LocalAppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppName);
+	}
+}
