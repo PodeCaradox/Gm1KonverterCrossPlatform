b73206172ab85026ff9ac3cb60b5967bebd77608
StrongholdOverlordsMod <strongholdoverlordsmod@gmail.com>
2021-08-31 20:04:22 +0200

    Reduced unnecesary code

--- Gm1KonverterCrossPlatform/Views/ChangeColorPalette.axaml.cs
+++ Gm1KonverterCrossPlatform/Views/ChangeColorPalette.axaml.cs
@@ -13,54 +13,39 @@ namespace Gm1KonverterCrossPlatform.HelperClasses.Views
 {
     public class ChangeColorPalette : Window
     {
-        private static readonly int pixelsize = 20;
+        private const int pixelsize = 20;
+
+        private Image image;
+        private Rectangle highlight;
+        private int positionInPalette;
+
         public ChangeColorPalette()
         {
             this.InitializeComponent();
-           
+
             this.Closing += WindowClosed;
             image = this.Get<Image>("PaletteImage");
-            canvas = this.Get<Canvas>("Cnv");
             image.PointerPressed += MousePressed;
-            var color = Color.FromArgb(100,255,0,0);
-            rect[0] = new Rectangle();
-            rect[0].Width = pixelsize;
-            rect[0].Height = 2;
-            rect[0].Fill = new SolidColorBrush(color);
-            canvas.Children.Add(rect[0]);
-            rect[1] = new Rectangle();
-            rect[1].Width = pixelsize;
-            rect[1].Height = 2;
-            rect[1].Fill = new SolidColorBrush(color);
-            canvas.Children.Add(rect[1]);
-            rect[2] = new Rectangle();
-            rect[2].Width = 2;
-            rect[2].Height = pixelsize - 4;
-            rect[2].Fill = new SolidColorBrush(color);
-            canvas.Children.Add(rect[2]);
-            rect[3] = new Rectangle();
-            rect[3].Width = 2;
-            rect[3].Height = pixelsize - 4;
-            rect[3].Fill = new SolidColorBrush(color);
-            canvas.Children.Add(rect[3]);
+
+            Color color = Color.FromArgb(100, 255, 0, 0);
+
+            highlight = new Rectangle();
+            highlight.Width = pixelsize;
+            highlight.Height = pixelsize;
+            highlight.Stroke = new SolidColorBrush(color);
+            highlight.StrokeThickness = 2;
+
+            Canvas canvas = this.Get<Canvas>("Cnv");
+            canvas.Children.Add(highlight);
         }
 
-        Image image;
-        private Canvas canvas;
-        Rectangle[] rect = new Rectangle[4];
-        private int positionInPalette;
         private void MousePressed(object sender, PointerPressedEventArgs e)
         {
             var pos = e.GetPosition(image);
             var newPos = new Point(((int)pos.X) / pixelsize * pixelsize, ((int)pos.Y) / pixelsize * pixelsize);
-            Canvas.SetTop(rect[0], newPos.Y - 160);
-            Canvas.SetLeft(rect[0], newPos.X);
-            Canvas.SetTop(rect[1], newPos.Y - 160 + 18);
-            Canvas.SetLeft(rect[1], newPos.X);
-            Canvas.SetTop(rect[2], newPos.Y - 160 + 2);
-            Canvas.SetLeft(rect[2], newPos.X);
-            Canvas.SetTop(rect[3], newPos.Y - 160 + 2);
-            Canvas.SetLeft(rect[3], newPos.X + pixelsize - 2);
+            
+            Canvas.SetTop(highlight, newPos.Y - 160);
+            Canvas.SetLeft(highlight, newPos.X);
 
             var vm = DataContext as MainWindowViewModel;
             positionInPalette = (int)newPos.X / pixelsize + (int)(newPos.Y) / pixelsize * 32;
