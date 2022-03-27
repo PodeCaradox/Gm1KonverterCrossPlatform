0b9971055e2588d610fe7c2919e7b6c2730415d8
StrongholdOverlordsMod <strongholdoverlordsmod@gmail.com>
2022-03-27 12:37:48 +0200

    moved and updated methods

--- Gm1KonverterCrossPlatform/Files/TGXImage.cs
+++ Gm1KonverterCrossPlatform/Files/TGXImage.cs
@@ -43,37 +43,6 @@ namespace Gm1KonverterCrossPlatform.Files
             imgFileAsBytearray = array.ToArray();
         }
 
-        internal unsafe void CreateNoComppressionImageFromByteArray(Palette palette,int offset)
-        {
-            //-7 because the images only height -7 long idk why
-            bmp = new WriteableBitmap(
-                new Avalonia.PixelSize(Header.Width, Header.Height - offset),
-                new Avalonia.Vector(96, 96),
-                Avalonia.Platform.PixelFormat.Bgra8888 // Bgra8888 is device-native and much faster.
-            );
-
-            using (var buf = bmp.Lock())
-            {
-                byte r, g, b, a;
-                uint x = 0;
-                uint y = 0;
-                for (int bytePos = 0; bytePos < imgFileAsBytearray.Length; bytePos+=2)
-                {
-                    Utility.ReadColor(BitConverter.ToUInt16(imgFileAsBytearray, bytePos), out r, out g, out b, out a);
-                    var colorByte = (UInt32)(b | (g << 8) | (r << 16) | (a << 24));
-                    var ptr = (uint*)buf.Address;
-                    ptr += (uint)((Header.Width * y) + x);
-                    *ptr = colorByte;
-                    x++;
-                    if (x == Header.Width)
-                    {
-                        y++;
-                        x = 0;
-                    }
-                }
-            }
-        }
-
         /// <summary>
         /// Convert imported Imgs without a Pallete to Byte array to safe new GM1 File
         /// </summary>
@@ -83,28 +52,8 @@ namespace Gm1KonverterCrossPlatform.Files
         internal void ConvertImageWithoutPaletteToByteArray(List<ushort> colors, int width, int height)
         {
             var array = Utility.ImgToGM1ByteArray(colors, width, height, Header.AnimatedColor);
-            //for (int i = 0; i < imgFileAsBytearray.Length; i++)
-            //{
-            //    if (imgFileAsBytearray[i]!= array[i])
-            //    {
-
-            //    }
-            //}
 
             imgFileAsBytearray = array.ToArray();
-        }
-
-        internal void ConvertNoCommpressionImageToByteArray(List<ushort> list, int width, int height)
-        {
-            List<byte> newArray = new List<byte>();
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    newArray.AddRange(BitConverter.GetBytes(list[y * width + x]));
-                }
-            }
-            imgFileAsBytearray = newArray.ToArray();
         }
     }
 }
